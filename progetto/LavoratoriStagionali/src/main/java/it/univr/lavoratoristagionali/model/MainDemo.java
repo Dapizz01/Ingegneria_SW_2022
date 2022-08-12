fa0331cc93168306098bcbe2dd6574181fc3c9bf
random <manuel.sb@libero.it>
2022-08-12 21:01:01 +0200

    Collegamento al database funzionante

--- progetto/LavoratoriStagionali/src/main/java/it/univr/lavoratoristagionali/model/MainDemo.java
+++ progetto/LavoratoriStagionali/src/main/java/it/univr/lavoratoristagionali/model/MainDemo.java
@@ -1,7 +1,18 @@
 package it.univr.lavoratoristagionali.model;
 
+import java.sql.*;
+
 public class MainDemo {
-    public static void main(String[] args) {
-        System.out.println("Ciao mondo");
+    public static void main( String args[] ) {
+        Connection c = null;
+
+        try {
+            Class.forName("org.sqlite.JDBC");
+            c = DriverManager.getConnection("jdbc:sqlite:test.db");
+        } catch ( Exception e ) {
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+            System.exit(0);
+        }
+        System.out.println("Opened database successfully");
     }
 }

087bb72ce52a7552884bf315a52e307e7eb42403
Dapizz01 <m1.dapozzo@gmail.com>
2022-08-21 01:20:20 +0200

    Refactor della UI con HBox e VBox Con implementazione di errori con messaggio (non ancora totalmente funzionante come il commit precedente)

--- progetto/LavoratoriStagionali/src/main/java/it/univr/lavoratoristagionali/controller/InserisciLavoratoriController.java
+++ progetto/LavoratoriStagionali/src/main/java/it/univr/lavoratoristagionali/controller/InserisciLavoratoriController.java
@@ -12,7 +12,9 @@ import javafx.collections.ObservableMap;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Border;
 import javafx.scene.text.Text;
 
 import java.net.URL;
@@ -21,39 +23,75 @@ import java.util.List;
 import java.util.ResourceBundle;
 
 public class InserisciLavoratoriController extends Controller implements Initializable {
+    // ANAGRAFICA
+    @FXML
+    private Text anagraficaTitle;
+    @FXML
+    private MFXTextField nomeLavoratore, cognomeLavoratore;
+    @FXML
+    private MFXDatePicker dataNascitaLavoratore;
+    @FXML
+    private MFXFilterComboBox<Comune> comuneNascitaLavoratore;
+    @FXML
+    private MFXCheckListView<Lingua> nazionalitaLavoratore;
+    @FXML
+    private Label nomeLavoratoreError, cognomeLavoratoreError, dataNascitaLavoratoreError, comuneNascitaLavoratoreError;
+
+    // CONTATTO LAVORATORE
+    @FXML
+    private MFXTextField indirizzoLavoratore, telefonoLavoratore, emailLavoratore;
+    @FXML
+    private Label indirizzoLavoratoreError, telefonoLavoratoreError, emailLavoratoreError;
+
+    // INFORMAZIONI GENERALI
+    @FXML
+    private MFXCheckListView<Lingua> lingueParlate;
     @FXML
     private MFXCheckListView<Patente> patentiPossedute;
-
     @FXML
-    private MFXCheckListView<Lingua> lingueParlate, nazionalitaLavoratore;
+    private MFXCheckbox automunito;
 
+    // CONTATTI URGENTI
+    @FXML
+    private MFXTextField nomeContatto, cognomeContatto, telefonoContatto, emailContatto;
+    @FXML
+    private MFXButton aggiungiContatto, eliminaContatto;
     @FXML
     private MFXListView<Contatto> listaContattoUrgente;
-
     @FXML
-    private MFXDatePicker dataNascita, inizioPeriodoDisponibilita, finePeriodoDisponibilita, inizioPeriodoEsperienza, finePeriodoEsperienza;
+    private Label nomeContattoError, cognomeContattoError, telefonoContattoError, emailContattoError;
 
+    // DISPONIBILITA
     @FXML
-    private MFXTextField nomeTextField, cognomeTextField, telefonoTextField, indirizzoTextField, emailTextField, nomeContattoUrgente, cognomeContattoUrgente, telefonoContattoUrgente, emailContattoUrgente, retribuzioneEsperienza, aziendaEsperienza;
-
+    private MFXDatePicker inizioDisponibilita, fineDisponibilita;
     @FXML
-    private MFXFilterComboBox<Comune> comuneNascita, comuneDisponibilita, comuneEsperienza;
-
+    private MFXFilterComboBox<Comune> comuneDisponibilita;
     @FXML
-    private Text anagraficaTitle, contattiTitle, informazioniGeneraliTitle, automunitoTitle;
-
-    @FXML
-    private MFXButton indietroButton, inviaLavoratoreButton, contattoUrgenteButton, eliminaContattoButton, eliminaDisponibilitaButton, aggiungiDisponibilitaButton, aggiungiEsperienzaButton, eliminaEsperienzaButton;
-
-    @FXML
-    private MFXCheckbox automunitoCheckBox;
-
+    private MFXButton aggiungiDisponibilita, eliminaDisponibilita;
     @FXML
     private MFXListView<Disponibilita> listaDisponibilita;
+    @FXML
+    private Label inizioDisponibilitaError, fineDisponibilitaError, comuneDisponibilitaError;
 
+    // ESPERIENZE
+    @FXML
+    private MFXTextField aziendaEsperienza, retribuzioneEsperienza;
+    @FXML
+    private MFXDatePicker inizioEsperienza, fineEsperienza;
+    @FXML
+    private MFXFilterComboBox<Comune> comuneEsperienza;
+    @FXML
+    private MFXButton aggiungiEsperienza, eliminaEsperienza;
     @FXML
     private MFXListView<Esperienza> listaEsperienze;
+    @FXML
+    private Label aziendaEsperienzaError, retribuzioneEsperienzaError, inizioEsperienzaError, fineEsperienzaError, comuneEsperienzaError;
 
+    // ... il resto
+    @FXML
+    private MFXButton ritornaMenu, inviaLavoratore;
+
+    // Variabili del controller non legate ad FXML
     private ObservableList<Contatto> contatti;
 
     private ObservableList<Esperienza> esperienze;
@@ -71,7 +109,7 @@ public class InserisciLavoratoriController extends Controller implements Initial
         List<Lingua> nazionalita = List.of(new Lingua(1, "nazionalità1"), new Lingua(2, "nazionalità2"), new Lingua(3, "nazionalità3"));
         List<Patente> patenti = List.of(new Patente(1, "patente1"), new Patente(2, "patente2"), new Patente(3, "patente3"));
 
-        comuneNascita.setItems(FXCollections.observableArrayList(comuni));
+        comuneNascitaLavoratore.setItems(FXCollections.observableArrayList(comuni));
         lingueParlate.setItems(FXCollections.observableArrayList(lingue));
         nazionalitaLavoratore.setItems(FXCollections.observableArrayList(nazionalita));
         patentiPossedute.setItems(FXCollections.observableArrayList(patenti));
@@ -92,20 +130,40 @@ public class InserisciLavoratoriController extends Controller implements Initial
         disponibilita = FXCollections.observableArrayList(new ArrayList<>());
         listaDisponibilita.setItems(disponibilita);
 
-        buildTextFieldValidator(nomeTextField, Errore.LETTERS_ONLY, Errore.NON_EMPTY);
+        buildTextFieldValidator(nomeLavoratore, Errore.LETTERS_ONLY, Errore.NON_EMPTY);
+
+        // TEST ERRORE
+
+        nomeLavoratore.delegateFocusedProperty().addListener((observable, oldValue, newValue) -> {
+            if(oldValue && !newValue){
+                List<Constraint> constraints = nomeLavoratore.validate();
+                if(!constraints.isEmpty()){
+                    nomeLavoratoreError.setText(constraints.get(0).getMessage());
+                    nomeLavoratoreError.setVisible(true);
+                    nomeLavoratore.setStyle("-fx-border-color: -mfx-red");
+                }
+            }
+        });
+
+        nomeLavoratore.getValidator().validProperty().addListener((observable, oldValue, newValue) -> {
+            if(newValue){
+                nomeLavoratore.setStyle("-fx-border-color: -mfx-green");
+                nomeLavoratoreError.setVisible(false);
+            }
+        });
         // TODO: aggiungere stili css errori con label di errore associata
         // Link utili: https://github.com/palexdev/MaterialFX/blob/main/demo/src/main/java/io/github/palexdev/materialfx/demo/controllers/TextFieldsController.java
         // https://github.com/palexdev/MaterialFX/blob/main/demo/src/main/resources/io/github/palexdev/materialfx/demo/css/TextFields.css
     }
 
     @FXML
-    private void ritornaMenu(ActionEvent actionEvent){
+    private void onClickRitornaMenu(ActionEvent actionEvent){
         System.out.println("ritornaMenu fired");
         switchScene(getStageFromEvent(actionEvent), View.MAIN_MENU);
     }
 
     @FXML
-    private void inviaLavoratore(ActionEvent actionEvent){
+    private void onClickInviaLavoratore(ActionEvent actionEvent){
         /*System.out.println(lingueParlate.getSelectionModel().getSelectedValues());
         System.out.println(nazionalitaLavoratore.getSelectionModel().getSelectedValues());
         System.out.println(patentiPossedute.getSelectionModel().getSelectedValues());
@@ -113,19 +171,21 @@ public class InserisciLavoratoriController extends Controller implements Initial
 
 
         // TODO: aggiungere comune di abitazione
-        Lavoratore lavoratore = new Lavoratore(-1, nomeTextField.getText(), cognomeTextField.getText(), comuneNascita.getValue(), null, (int) dataNascita.getValue().toEpochDay(), nazionalitaLavoratore.getSelectionModel().getSelection().get(0), emailTextField.getText(), telefonoTextField.getText(), automunitoCheckBox.isSelected(), listaEsperienze.getItems(), lingueParlate.getItems(), listaContattoUrgente.getItems(), patentiPossedute.getItems(), listaDisponibilita.getItems());
+        Lavoratore lavoratore = new Lavoratore(-1, nomeLavoratore.getText(), cognomeLavoratore.getText(), comuneNascitaLavoratore.getValue(), null, (int) dataNascitaLavoratore.getValue().toEpochDay(), nazionalitaLavoratore.getSelectionModel().getSelection().get(0), emailLavoratore.getText(), telefonoLavoratore.getText(), automunito.isSelected(), listaEsperienze.getItems(), lingueParlate.getItems(), listaContattoUrgente.getItems(), patentiPossedute.getItems(), listaDisponibilita.getItems());
         System.out.println(lavoratore);
     }
 
     @FXML
-    private void onClickUrgenteButton(ActionEvent actionEvent) {
-        Contatto nuovoContatto = new Contatto(-1, nomeContattoUrgente.getText(), cognomeContattoUrgente.getText(), telefonoContattoUrgente.getText(), emailContattoUrgente.getText());
+    private void onClickAggiungiContatto(ActionEvent actionEvent) {
+        Contatto nuovoContatto = new Contatto(-1, nomeContatto.getText(), cognomeContatto.getText(), telefonoContatto.getText(), emailContatto.getText());
         contatti.add(nuovoContatto);
 
-        nomeContattoUrgente.clear();
-        cognomeContattoUrgente.clear();
-        telefonoContattoUrgente.clear();
-        emailContattoUrgente.clear();
+        nomeContatto.clear();
+        cognomeContatto.clear();
+        telefonoContatto.clear();
+        emailContatto.clear();
+
+        System.out.println(nomeLavoratore.validate());
     }
 
     @FXML
@@ -144,7 +204,7 @@ public class InserisciLavoratoriController extends Controller implements Initial
     }
 
     @FXML
-    private void eliminaContatto(ActionEvent actionEvent){
+    private void onClickEliminaContatto(ActionEvent actionEvent){
         for(int key : listaContattoUrgente.getSelectionModel().getSelection().keySet()){
             contatti.remove(listaContattoUrgente.getSelectionModel().getSelection().get(key));
         }
@@ -153,7 +213,7 @@ public class InserisciLavoratoriController extends Controller implements Initial
     }
 
     @FXML
-    private void eliminaDisponibilita(ActionEvent actionEvent) {
+    private void onClickEliminaDisponibilita(ActionEvent actionEvent) {
         for(int key : listaDisponibilita.getSelectionModel().getSelection().keySet()){
             disponibilita.remove(listaDisponibilita.getSelectionModel().getSelection().get(key));
         }
@@ -162,33 +222,33 @@ public class InserisciLavoratoriController extends Controller implements Initial
     }
 
     @FXML
-    private void aggiungiDisponibilita(ActionEvent actionEvent) {
-        Disponibilita nuovaDisponibilita = new Disponibilita((int) inizioPeriodoDisponibilita.getValue().toEpochDay(), (int) finePeriodoDisponibilita.getValue().toEpochDay(), comuneDisponibilita.getValue());
+    private void onClickAggiungiDisponibilita(ActionEvent actionEvent) {
+        Disponibilita nuovaDisponibilita = new Disponibilita((int) inizioDisponibilita.getValue().toEpochDay(), (int) fineDisponibilita.getValue().toEpochDay(), comuneDisponibilita.getValue());
         disponibilita.add(nuovaDisponibilita);
 
         System.out.println(disponibilita);
         System.out.println(disponibilita.get(0).getInizioPeriodo());
         System.out.println(disponibilita.get(0).getFinePeriodo());
 
-        inizioPeriodoDisponibilita.clear();
-        finePeriodoDisponibilita.clear();
+        inizioDisponibilita.clear();
+        fineDisponibilita.clear();
         comuneDisponibilita.clear();
     }
 
     @FXML
-    private void aggiungiEsperienza(ActionEvent actionEvent) {
-        Esperienza nuovaEsperienza = new Esperienza(-1, aziendaEsperienza.getText(), Integer.parseInt(retribuzioneEsperienza.getText()), (int) inizioPeriodoEsperienza.getValue().toEpochDay(), (int) inizioPeriodoEsperienza.getValue().toEpochDay(), comuneEsperienza.getValue());
+    private void onClickAggiungiEsperienza(ActionEvent actionEvent) {
+        Esperienza nuovaEsperienza = new Esperienza(-1, aziendaEsperienza.getText(), Integer.parseInt(retribuzioneEsperienza.getText()), (int) inizioEsperienza.getValue().toEpochDay(), (int) inizioEsperienza.getValue().toEpochDay(), comuneEsperienza.getValue());
         esperienze.add(nuovaEsperienza);
 
         aziendaEsperienza.clear();
         retribuzioneEsperienza.clear();
-        inizioPeriodoEsperienza.clear();
-        finePeriodoEsperienza.clear();
+        inizioEsperienza.clear();
+        fineEsperienza.clear();
         comuneEsperienza.clear();
     }
 
     @FXML
-    private void eliminaEsperienza(ActionEvent actionEvent) {
+    private void onClickEliminaEsperienza(ActionEvent actionEvent) {
         for(int key : listaEsperienze.getSelectionModel().getSelection().keySet()){
             esperienze.remove(listaEsperienze.getSelectionModel().getSelection().get(key));
         }
@@ -203,11 +263,13 @@ public class InserisciLavoratoriController extends Controller implements Initial
                     .setMessage(flag.getLabel())
                     .setCondition(Bindings.createBooleanBinding(() -> switch(flag){
                                 case NON_EMPTY:
-                                    yield textField.getText().equals("");
+                                    yield !textField.getText().equals("");
                                 case NUMBERS_ONLY:
                                     yield textField.getText().chars().allMatch(Character::isDigit);
-                                default:
+                                case LETTERS_ONLY:
                                     yield textField.getText().chars().allMatch(Character::isLetter);
+                                default:
+                                    yield true;
                             }, textField.textProperty())
                     ).get());
         }
